53c4d8a653c25672fa62d20ec56eefda9dc4dc29
Leandro A. Mendez <leandroa.mendez@gmail.com>
2023-07-07 16:50:16 -0300

    arreglados margenes de los titulos del hero

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -2,9 +2,9 @@ import Link from "../ui-components/Link"
 
 const Hero = () => {
   return (
-    <div className=" flex flex-col">
-<h1 className={`sm:text-6xl text-4xl font-bold mb-5 opacity-90`}>Leandro Méndez</h1>
-<h2 className={`text-2xl font-medium mb-5 opacity-90`}>Full Stack Developer</h2>
+    <div className=" flex flex-col gap-2">
+<h1 className={`sm:text-6xl text-4xl font-bold  opacity-90`}>Leandro Méndez</h1>
+<h2 className={`text-2xl font-medium  opacity-90`}>Full Stack Developer</h2>
 <p className={`text-base font-light tracking-widest max-w-xs mb-6`}>Crafting inclusive and innovative web solutions.</p>
     </div>
   )
