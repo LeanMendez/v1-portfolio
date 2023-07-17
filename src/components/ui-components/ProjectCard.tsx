388ceaee4504da8092025e6293b7d3527298655e
Leandro A. Mendez <leandroa.mendez@gmail.com>
2023-07-17 17:51:32 -0300

    fix Link hover en el componente projectCard

--- src/components/ui-components/ProjectCard.tsx
+++ src/components/ui-components/ProjectCard.tsx
@@ -8,14 +8,15 @@ import { IProjects } from "@/types";
 
 const ProjectCard:React.FC<IProjects>= (props) => {
   return (
-    <div
-      className={`group/link lg:h-min-[250px] lg:w-[518px] border-[1px] border-slate-200 border-opacity-10 sm:hover:shadow-muteNeon p-3 lg:p-8 rounded-lg sm:hover:bg-slate-50 sm:hover:bg-opacity-5 bg-clip-padding backdrop-filter backdrop-blur-sm `}
-    >
-      <Link
+    <Link
       className={`h-6 w-6 text-slate-200 no-underline`}
       href={props.external ? props.external  : props.github}
       icon={false}
-      ><span className={`text-2xl leading-6 duration-300 inline-block mb-4`}>{props.title}</span>
+      >
+    <div
+      className={`group/link lg:h-min-[250px] lg:w-[518px] border-[1px] border-slate-200 border-opacity-10 sm:hover:shadow-muteNeon p-3 lg:p-8 rounded-lg sm:hover:bg-slate-50 sm:hover:bg-opacity-5 bg-clip-padding backdrop-filter backdrop-blur-sm `}
+    >
+      <span className={`text-2xl leading-6 duration-300 inline-block mb-4`}>{props.title}</span>
       <div className={`flex flex-row`}>
       <aside className={`min-w-[120px] hidden sm:block`}>
         <Image className={` mt-2 rounded ease-in duration-100 grayscale group-hover/link:grayscale-0 sm:order-1 sm:col-span-2 sm:translate-y-1`} src={`/img/${props.image}`} alt={props.title} height={88} width={120} />
@@ -27,8 +28,8 @@ const ProjectCard:React.FC<IProjects>= (props) => {
         <div className={`flex flex-wrap mt-3 gap-2 max-w-[90%]`}>{props.tech.map(item=> <Pill key={item} str={item} />)}</div>
       </main>
       </div>
-      </Link>
     </div>
+    </Link>
   );
 };
 
